ccaf51d4b5e45d1109c5f67f05a4ae41440877a0
Will Singleton <will5horace@ku.edu>
2025-03-26 22:29:23 -0500

    Beginning of the football functionality, with defining the scores
    Next we add the two main functions, counting the number of combinations
    and printing all of the combinations out

--- football.c
+++ football.c
@@ -5,9 +5,42 @@ int TD = 6; //Touch Down = 6Pts
 int FG = 3;  //Field Goal = 3Pts
 int ST = 2;  //Safety = 2Pts
 int TD_2 = 8;  //Touch Down with 2Pts = 8
-int TD_FG1 = 7;  //Touch Down with 
+int TD_FG1 = 7;  //Touch Down with FG 1Pts = 7
 
-int main() { 
-    int score;
-    
+void print_combination(int score) {  //Finds all the possibilties of the score with different scoring methods
+    for (int td2pt = 0; td2pt * TD_2 <= score; td2pt++) {  /*Starting from zero we interate through all the combos 
+        of what could equal the score */
+        for(int td_fg = 0; td_fg * TD_FG1 <= score; td_fg++) {  
+            for(int td = 0; td * TD <= score; td++)  {  // The ammount of times is added to the variable 
+                for(int fg = 0; fg * FG <=score; fg++) {
+                    for(int st = 0; st * ST <=score; st++) {  //We now know theough all of these, the coombinations
+                        int total = (td2pt * TD_2) + (td_fg * TD_FG1) + (td* TD) + (fg *FG) + (st * ST);
+                        if(total == score)  {  // Now we take the combinations and print them
+                            printf("%d TD + 2, %d TD + FG, %d TD, %d FG-3pt, %d Safety\n ", 
+                                td2pt, td_fg, td, fg, st);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+int count_combination(int score)  {
+    int count = 0;
+    for (int td2pt = 0; td2pt * TD_2 <= score; td2pt++) {  /*Starting from zero we interate through all the combos 
+        of what could equal the score */
+        for(int td_fg = 0; td_fg * TD_FG1 <= score; td_fg++) {  
+            for(int td = 0; td * TD <= score; td++)  {  // The ammount of times is added to the variable 
+                for(int fg = 0; fg * FG <=score; fg++) {
+                    for(int st = 0; st * ST <=score; st++) {  //We now know theough all of these, the coombinations
+                        int total = (td2pt * TD_2) + (td_fg * TD_FG1) + (td* TD) + (fg *FG) + (st * ST);
+                        if(total == score)  {  
+                            count++
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return count;
 }
