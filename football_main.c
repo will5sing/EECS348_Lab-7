c4b469b46e2b63a99af998a2cb9e8abcc9b331d3
Will Singleton <will5horace@ku.edu>
2025-03-26 23:09:48 -0500

    This is where we tie everything together and use a while loop in which the user is repeatedly asked whether or not they want to continue, provided some error checking. Next, we ask the user for the input and then put that score into the already made functions from football.h

--- football_main.c
+++ football_main.c
@@ -1,5 +1,24 @@
 #include "football.h"
 
 int main(int argc, char *argv[]) {
-    return 0;
+    int score, option, continue_loop; //Creating the variable of the score the user wants
+    while (option != 1) {
+        printf("Enter 0 or 1(Go or Stop): ");  //User while loop, that keeps giving them the score until 0
+        scanf( "%d", &continue_loop);
+        if (continue_loop == 1) {  //We look at whether the user wants to continue
+            printf("Enter the NFL Score: ");  //Ask the user for the desired scpre
+            scanf("%d", &score);
+            printf("Possible combinations of scoring plays for a team's score of %d: \n",score); //We then print just like the example
+            print_combinations(score); //Then we run the score through to get and print all the combinations
+            int count = count_combinations(score);
+            printf("Total combinations: %d\n", count);  //Just for extra info we print the number of all the combinations
+        }
+        if (continue_loop == 0) {  //This is to exit the program and end the while loop
+            break;
+        } 
+        else {  //This is for error handling
+            printf("Invalid Input");
+        }
+    }
+    return 0; //Here we end the main
 }
