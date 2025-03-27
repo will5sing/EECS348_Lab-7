9a820b983ae19b83b3499af56dfb8af9c9b8544b
Will Singleton <will5horace@ku.edu>
2025-03-26 21:50:01 -0500

    Beginning of the football functionality, with defining the scores

--- football.c
+++ football.c
@@ -1,1 +1,13 @@
 #include "football.h"
+#include <stdio.h>
+
+int TD = 6; //Touch Down = 6Pts
+int FG = 3;  //Field Goal = 3Pts
+int ST = 2;  //Safety = 2Pts
+int TD_2 = 8;  //Touch Down with 2Pts = 8
+int TD_FG1 = 7;  //Touch Down with 
+
+int main() { 
+    int score;
+    
+}
